01ce94192e4b2fde4f26f0445b74c58c4eef036a
Le Xuan Manh <lexm@garena.com>
2015-09-19 22:48:16 +0800

    added test for search command

--- TextBuddy/src/TextBuddy/TextBuddyTests.java
+++ TextBuddy/src/TextBuddy/TextBuddyTests.java
@@ -17,6 +17,12 @@ public class TextBuddyTests {
         TextBuddy.executeCommand("delete 1");
         assertEquals("All content was sorted", TextBuddy.executeCommand("sort"));
         assertEquals("1: test 0\n2: test 2\n3: test 3\n", TextBuddy.executeCommand("display"));
+        
+        //added test for searchText() method.
+        assertEquals("2: test 2\n", TextBuddy.executeCommand("search test 2"));
+        TextBuddy.executeCommand("add test 22");
+        assertEquals("2: test 2\n4: test 22\n", TextBuddy.executeCommand("search test 2"));
+        assertEquals("No line contains word CS2103T", TextBuddy.executeCommand("search CS2103T"));
     }
 
 }
