2c04c2fe51d215f73071c49d8683d4f24ac60f36
Le Xuan Manh <lexm@garena.com>
2015-09-19 22:26:02 +0800

    added test for sort command

--- TextBuddy/src/TextBuddy/TextBuddyTests.java
+++ TextBuddy/src/TextBuddy/TextBuddyTests.java
@@ -1,5 +1,22 @@
 package TextBuddy;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class TextBuddyTests {
+    
+    public void testTextBuddy() {
+        //added test for sortText() method.
+        TextBuddy.executeCommand("add test 1");
+        TextBuddy.executeCommand("add test 3");
+        TextBuddy.executeCommand("add test 2");
+        assertEquals("All content was sorted", TextBuddy.executeCommand("sort"));
+        assertEquals("1: test 1\n2: test 2\n3: test 3\n", TextBuddy.executeCommand("display"));
+        TextBuddy.executeCommand("add test 0");
+        TextBuddy.executeCommand("delete 1");
+        assertEquals("All content was sorted", TextBuddy.executeCommand("sort"));
+        assertEquals("1: test 0\n2: test 2\n3: test 3\n", TextBuddy.executeCommand("display"));
+    }
 
 }
